df480169c8f79a99e9ed45282d40097f70a43268
miniflycn <miniflycn@justany.net>
2013-12-08 23:32:50 +0800

    add test case

--- test/har.js
+++ test/har.js
@@ -2,21 +2,35 @@ var har = require('../lib/har')
   , connect = require('connect');
 
 var testServer = connect()
-                  .use(function (req, res, next) {
+                  .use('/available', function (req, res, next) {
                     res.writeHead(200, {
                       'Content-Type': 'text/html; charset=UTF-8'
                     });
                     res.statusCode = 200;
                     res.end('<html><head><title>test</title></head><body>hello world!</body></html>');
                   })
+                  .use('/unavailable', function (req, res, next) {
+                    res.writeHead(404, {
+                      'Content-Type': 'text/html; charset=UTF-8'
+                    });
+                    res.statusCode = 404;
+                    res.end('Page Not Found!');
+                  })
                   .listen(7777);
 
 describe('har', function () {
   it('should able to get a har data', function (done) {
-    var a = har('http://127.0.0.1:7777/').success(function (data) {
+    har('http://127.0.0.1:7777/available').success(function (data) {
       var page = data.log.pages[0];
       page.title.should.equal('test');
-      page.id.should.equal('http://127.0.0.1:7777/');
+      page.id.should.equal('http://127.0.0.1:7777/available');
+      done();
+    });
+  });
+
+  it('should occur a error', function (done) {
+    har('wrongaddress').fail(function (data) {
+      data.should.equal('ADDRESS is wrong!');
       done();
     });
   });
