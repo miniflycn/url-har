50cd684e388ed796e9948a4c4e5f01ab5aa43892
Daniel Yang <miniflycn@justany.net>
2013-12-09 17:57:09 +0800

    add a example

---
+++ example/fetch.js
@@ -0,0 +1,42 @@
+!function () { 
+
+  var har = require('../')
+    , fs = require('fs')
+    , spawn = require('child_process').spawn
+    , http = require('http');
+
+  var filePath = __dirname + '/har_to_pagespeed.exe'
+    , file;
+
+  // http://page-speed.googlecode.com/files/har_to_pagespeed.exe
+  fs.exists(filePath, function (exists) {
+    file = fs.createWriteStream(filePath);
+    http.get('http://page-speed.googlecode.com/files/har_to_pagespeed.exe', function (res) {
+      res.on('data', function (data) {
+        file.write(data);
+      }).on('end', function () {
+        file.end();
+        harUrl('http://www.qq.com');
+      });
+    });
+  });
+  
+  function harUrl(url) {
+    har(url)
+      .success(function (data) {
+        fs.writeFile('tmp.har', data, function () {
+          var worker = spawn('har_to_pagespeed', [__dirname + '/tmp.har'])
+            , that = this;
+          worker.stdout.on('data', function (data) {
+            console.log(data.toString());
+          });
+          worker.stderr.on('data', function (data) {
+            
+          });
+          worker.on('close', function (code) {
+            
+          });
+        });
+      });
+  }
+}();
--- test/har.js
+++ test/har.js
@@ -21,6 +21,7 @@ var testServer = connect()
 describe('har', function () {
   it('should able to get a har data', function (done) {
     har('http://127.0.0.1:7777/available').success(function (data) {
+      data = JSON.parse(data);
       var page = data.log.pages[0];
       page.title.should.equal('test');
       page.id.should.equal('http://127.0.0.1:7777/available');
@@ -32,6 +33,7 @@ describe('har', function () {
     var num = 0;
     har(['http://127.0.0.1:7777/available/1.html', 'http://127.0.0.1:7777/available/2.html'])
     .success(function (data) {
+      data = JSON.parse(data);
       data.log.pages[0].id.should.equal(num === 0 ? 'http://127.0.0.1:7777/available/1.html' : 'http://127.0.0.1:7777/available/2.html')
       if (++num === 2) return done();
     })
